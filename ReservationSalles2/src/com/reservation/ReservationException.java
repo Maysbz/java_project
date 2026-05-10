f37c21fcfe24272da26025d0987e98ccce8da772
Maysbz <benzekrimeys@gmail.com>
2026-05-10 16:02:08 +0100

    Delete ReservationSalles2 directory

--- ReservationSalles2/src/com/reservation/ReservationException.java
+++
@@ -1,8 +0,0 @@
-package com.reservation;
-
-// Exception personnalisée → Binôme 1
-public class ReservationException extends Exception {
-    public ReservationException(String message) {
-        super(message);
-    }
-}
--- ReservationSalles2/src/com/reservation/Sauvegardable.java
+++
@@ -1,9 +0,0 @@
-package com.reservation;
-
-import java.util.List;
-
-// Interface générique → Binôme 2
-public interface Sauvegardable<T> {
-    void sauvegarder(List<T> elements, String chemin) throws Exception;
-    List<T> charger(String chemin) throws Exception;
-}
